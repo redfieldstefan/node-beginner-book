118e0a3d4de936bce6e4fd4ea63d857c112c6c63
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-03 22:50:42 -0700

    add request handlers

---
+++ modules/requestHandlers.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var querystring = require('querystring');
+var fs = require('fs');
+var formidable = require('formidable');
+
+function start(res, postData) {
+	console.log('Request handler "start" was called');
+
+	var body = 
+		'<html>' + 
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html";' +
+		'charset= UTF-8 />' +
+		'</head>' +
+		'<body>' +
+		'<form action="/upload" enctype = "multipart/form-data" method="post">' +
+		'<input type = "file" name = "upload" multiple = "multiple">' +
+		'<input type="submit" value = "Upload file" />' +
+		'</form>' +
+		'</body>' +
+		'</html>';
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(body);
+	res.end();
+}
+
+function upload(res, req) {
+	console.log('Request handler "upload" was called');
+
+	var form = new formidable.IncomingForm();
+	console.log('about to parse');
+	form.parse(req, function(error, fields, files) {
+		console.log('parsing done');
+
+		fs.rename(files.upload.path, './tmp/test.png');
+			if (error) {
+				fs.unlink('./tmp/test.png');
+				fs.rename(files.upload.path, './tmp/test.png');
+			}
+	});
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('recieved image: <br/>');
+	res.write('<img src = "/show" />');
+	res.end();
+}
+
+function show(res) {
+	console.log('Request handler "show" was called');
+	res.writeHead(200, {'Content-Type': 'image/png'});
+	fs.createReadStream('./tmp/test.png').pipe(res);
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
+
