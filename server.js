d7deecc90fdb3f2989e108d8ee2842bd795926e8
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-03 22:52:19 -0700

    move server file to modules

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello World');
-	res.end();
-}).listen(8888);
